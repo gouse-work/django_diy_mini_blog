fe56fce147b30a0113491cd429bd736fd7112a13
Mahammad Gouse <msk.work91@gmail.com>
2021-12-30 17:56:22 +0530

    bugfix attempt

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-$r*4y=$p9!x7bnqx07g*p)i@ujc5nz2+7yx&97m$ao+(+fmvj-')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-$r*4y=$p9!x7bnqx07g*p)i@ujc5nz2+7yx&97m$ao+(+fmvj-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG','') != 'False'
